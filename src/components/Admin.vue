888bbfc435a03041993aa555c83d1d37f2820ae1
kmyles82 <kmyles82@gmail.com>
2019-08-06 23:23:47 -0500

    added Current Orders section

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,45 +1,69 @@
 <template>
+  <div>
     <div class="row">
-        <div class="col-sm12 col-md-6">
-            <!-- new pizza components -->
-        </div>
-        <div class="col-sm12 col-md-6">
-            <h3>Menu</h3>
-            <table class="table table-hover">
-                <thead class="thead-default">
-                    <tr>
-                        <th>
-                            Item
-                        </th>
-                        <th>
-                            Remove from menu
-                        </th>
-                    </tr>
-                </thead>
-                <tbody >
-                    <tr>
-                        <td>Margherita</td>
-                        <td>
-                            <button class="btn btn-sm btn-outline-danger">X</button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+      <div class="col-sm12 col-md-6">
+        <!-- new pizza components -->
+      </div>
+      <div class="col-sm12 col-md-6">
+        <h3>Menu</h3>
+        <table class="table table-hover">
+          <thead class="thead-default">
+            <tr>
+              <th>Item</th>
+              <th>Remove from menu</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Margherita</td>
+              <td>
+                <button class="btn btn-sm btn-outline-danger">X</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
+    <div class="row">
+      <div class="col-sm-12">
+        <h3>Current orders:</h3>
+        <table class="table table-sm">
+          <thead class="thead-default">
+            <tr>
+              <th>Item</th>
+              <th>Size</th>
+              <th>Quantity</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <div class="order-number">
+            <strong>
+              <em>Order Number: 1</em>
+            </strong>
+            <button class="btn btn-sm btn-outline-danger">X</button>
+          </div>
+            <tbody>
+              <tr>
+                <td>Margherita</td>
+                <td>9"</td>
+                <td>1</td>
+                <td>6.95</td>
+              </tr>
+            </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Admin',
-    data(){
-        return{
-
-        }
-    }
-}
+  name: "Admin",
+  data() {
+    return {};
+  }
+};
 </script>
 
 <style>
-
 </style>

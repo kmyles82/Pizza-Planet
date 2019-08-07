7a3a97271d6436ce6b94fc3a380d44843e39cc1a
kmyles82 <kmyles82@gmail.com>
2019-08-07 16:01:58 -0500

    setup state and getters in store

--- src/store.js
+++ src/store.js
@@ -5,12 +5,58 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    menuItems: {
+      1: {
+        name: 'Magherita',
+        discription: 'A delicious tomato based pizza topped with mozzarella',
+        options: [{
+            size: 9,
+            price: 6.95
+          },
+          {
+            size: 12,
+            price: 10.95
+          }
+        ]
+      },
+      2: {
+        name: 'Pepperoni',
+        discription: 'A delicious tomato based pizza topped with mozzarella and pepperoni',
+        options: [{
+            size: 9,
+            price: 7.95
+          },
+          {
+            size: 12,
+            price: 12.95
+          }
+        ]
+      },
+      3: {
+        name: 'Ham and Pineapple',
+        discription: 'A delicious tomato based pizza topped with mozzarella, ham and pineapple',
+        options: [{
+            size: 9,
+            price: 7.95
+          },
+          {
+            size: 12,
+            price: 12.95
+          }
+        ]
+      }
+    },
+    orders: [],
   },
   mutations: {
 
   },
   actions: {
 
+  },
+  getters: {
+    getMenuItems: state =>  state.menuItems,
+    numberOfOrders: state => state.orders.length,
+    
   }
 })

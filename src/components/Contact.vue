74d1897cc62821d7fb2376f8e5a323ea6601c76a
kmyles82 <kmyles82@gmail.com>
2019-08-07 12:35:38 -0500

    created contact, delivery, orderingguide and history components

---
+++ src/components/Contact.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="card">
+    <div class="card-block">
+      <h4 class="card-title">Contact</h4>
+      <address>
+        <strong>Pizza Planet</strong>
+        <br />Address line 1
+        <br />Address line 2
+        <br />Phone: (123-456-7890)
+      </address>
+      <address>
+        <strong>Email:</strong>
+        <br />
+        <p>contact@pizzaplanet.com</p>
+      </address>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Contact",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/OrderGuide.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card">
+    <div class="card-block">
+      <h4 class="card-title">How to order</h4>
+      <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, quis provident, molestiae error commodi consequatur voluptas aliquid non laborum blanditiis molestias vero maxime sapiente nihil atque dolores eligendi, exercitationem sequi.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "OrderingGuide",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+</style>

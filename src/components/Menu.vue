7c208c2ea4a89e92a300303f44cfc006e5306734
kmyles82 <kmyles82@gmail.com>
2019-08-06 22:59:05 -0500

    added Basket section

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -17,11 +17,44 @@
                         <td>{{option.size}}"</td>
                         <td>{{option.price}}</td>
                         <td>
-                            <button class="btn btn-success btn-sm btn-outline" type="button">+</button>
+                            <button 
+                                class="btn btn-success btn-sm btn-outline-success" 
+                                type="button"
+                                v-on:click.prevent="addToBasket(pizza, option)">+</button>
                         </td>
                     </tr>
                 </tbody>
             </table>
+        </div>
+        <!-- Shopping Basket -->
+        <div class="col-sm-12 col-md-6" >
+            <div v-if="basket.length > 0">
+                <table class="table table">
+                    <thead class="thead-default">
+                        <tr>
+                            <th>Quantity</th>
+                            <th>Item</th>
+                            <th>Total</th>
+                        </tr>
+                    </thead>
+                    <tbody v-for="(item, index) in basket" :key="index">
+                        <tr>
+                            <td>
+                                <button class="btn btn-sm" type="button">-</button>
+                                <span>{{item.quantity}}</span>
+                                <button class="btn btn-sm" type="button">+</button>
+                            </td>
+                            <td>{{item.name}} {{item.size}}"</td>
+                            <td>{{item.price * item.quantity}}</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <p>Order total: {{}}</p>
+                <button class="btn btn-success btn-block">Place Order</button>
+            </div>
+            <div v-else>
+                <p>{{basketText}}</p>
+            </div>
         </div>
     </div>
 </template>
@@ -31,6 +64,8 @@ export default {
     name: 'Menu',
     data(){
         return{
+            basket: [],
+            basketText: 'Your basket is empty',
             getMenuItems: {
                 1: {
                     name: 'Magherita',
@@ -73,10 +108,20 @@ export default {
                             price: 12.95
                         }
                     ]
-                },
+                }
             }
         }
-    }
+    },
+    methods:{
+        addToBasket(pizza, option){
+            this.basket.push({
+                name: pizza.name,
+                price: option.price,
+                size: option.size,
+                quantity: 1
+            })
+        }
+    },
 }
 </script>
 

02ddbbe69851663c6d3915e131e740f8ad06bc1d
kmyles82 <kmyles82@gmail.com>
2019-08-06 23:17:28 -0500

    created Admin component

---
+++ src/components/Admin.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="row">
+        <div class="col-sm12 col-md-6">
+            <!-- new pizza components -->
+        </div>
+        <div class="col-sm12 col-md-6">
+            <h3>Menu</h3>
+            <table class="table table-hover">
+                <thead class="thead-default">
+                    <tr>
+                        <th>
+                            Item
+                        </th>
+                        <th>
+                            Remove from menu
+                        </th>
+                    </tr>
+                </thead>
+                <tbody >
+                    <tr>
+                        <td>Margherita</td>
+                        <td>
+                            <button class="btn btn-sm btn-outline-danger">X</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Admin',
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>

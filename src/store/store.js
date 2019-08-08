1591e05ab95eb56305376dc0c88e669ab3282185
kmyles82 <kmyles82@gmail.com>
2019-08-08 12:59:32 -0500

    refactored store into seperate modules instead

--- src/store/store.js
+++ src/store/store.js
@@ -1,58 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import * as getters from './getters'
-import * as actions from './actions'
-import * as mutations from './mutations'
+import menu from './modules/menu'
+import orders from './modules/orders'
+import users from './modules/users'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    menuItems: {
-      1: {
-        name: 'Magherita',
-        discription: 'A delicious tomato based pizza topped with mozzarella',
-        options: [{
-            size: 9,
-            price: 6.95
-          },
-          {
-            size: 12,
-            price: 10.95
-          }
-        ]
-      },
-      2: {
-        name: 'Pepperoni',
-        discription: 'A delicious tomato based pizza topped with mozzarella and pepperoni',
-        options: [{
-            size: 9,
-            price: 7.95
-          },
-          {
-            size: 12,
-            price: 12.95
-          }
-        ]
-      },
-      3: {
-        name: 'Ham and Pineapple',
-        discription: 'A delicious tomato based pizza topped with mozzarella, ham and pineapple',
-        options: [{
-            size: 9,
-            price: 7.95
-          },
-          {
-            size: 12,
-            price: 12.95
-          }
-        ]
-      }
-    },
-    orders: [],
-    currentUser: null,
-  },
-  mutations,
-  actions,
-  getters
+  modules: {
+    menu,
+    orders,
+    users
+  }
 })
